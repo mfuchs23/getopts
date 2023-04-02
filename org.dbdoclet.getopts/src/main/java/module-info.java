63ef46418b7f4ade839d5033a2662d121a870a76
Michael Fuchs <michael.fuchs@dbdoclet.org>
2023-04-02 12:27:07 +0200

    Migration to new Doclet API

--- org.dbdoclet.getopts/src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module org.dbdoclet.getopts {
-	requires org.dbdoclet.commons;
-	exports org.dbdoclet.option;
-}
--- org.dbdoclet.getopts/src/test/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.dbdoclet.getopts {
-	
-	requires org.dbdoclet.commons;
-	requires org.junit.jupiter.api;
-
-	exports org.dbdoclet.option;
-}
